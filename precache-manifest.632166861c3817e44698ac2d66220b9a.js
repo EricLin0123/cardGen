2ff89b8d42f72ab2d2ea8fa24ba96e812744f213
chuan <150107jasmine@gmail.com>
2023-12-05 23:38:44 +0800

    Updates

---
+++ precache-manifest.632166861c3817e44698ac2d66220b9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bc12cb7cc01bd1ccea7abc1eb8c99d3",
+    "url": "/cardGen/index.html"
+  },
+  {
+    "revision": "afeb770c56a2243d9377",
+    "url": "/cardGen/static/css/main.ef4bdfba.chunk.css"
+  },
+  {
+    "revision": "a60ce7ae2692164106bb",
+    "url": "/cardGen/static/js/2.cd8ff0bc.chunk.js"
+  },
+  {
+    "revision": "5f54ce911b9b7fbc7fa8",
+    "url": "/cardGen/static/js/3.9045e5ae.chunk.js"
+  },
+  {
+    "revision": "afeb770c56a2243d9377",
+    "url": "/cardGen/static/js/main.deb800ec.chunk.js"
+  },
+  {
+    "revision": "6864ab8ad25a8003441d",
+    "url": "/cardGen/static/js/runtime~main.d1d9a23e.js"
+  }
+]);
